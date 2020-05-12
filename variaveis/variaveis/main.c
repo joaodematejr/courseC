b59d7ae4b3378591e785fe0dafbf3841037da1d1
joaodematejr <joaodematejr@gmail.com>
2020-05-12 00:23:16 -0300

    Trabalhando Com Variavel

--- variaveis/variaveis/main.c
+++ variaveis/variaveis/main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  variaveis
+//  Aula Sobre Variáveis
 //
 //  Created by João Dematé Jr 🤠 on 11/05/20.
 //  Copyright © 2020 João Dematé Jr 🤠. All rights reserved.
@@ -9,7 +9,15 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    //DECLARANDO UMA VARIAVEL
+    int idade;
+    //DECLARANDO E INICIALIZANDO UMA VARIAVEL
+    //int idade = 0;
+    
+    printf("Qual a sua idade ? ");
+    scanf("%d", &idade);
+    
+    printf("A sua idade é %d anos \n", idade);
+    
     return 0;
 }
