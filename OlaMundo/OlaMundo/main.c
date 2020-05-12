b142f3c42e6659c76beb4d259f31f86af0710d21
joaodematejr <joaodematejr@gmail.com>
2020-05-11 23:43:25 -0300

    Primeiro Codigo Ola Mundo

---
+++ OlaMundo/OlaMundo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OlaMundo
+//
+//  Created by João Dematé Jr 🤠 on 11/05/20.
+//  Copyright © 2020 João Dematé Jr 🤠. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
