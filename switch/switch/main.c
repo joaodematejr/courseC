5d764c14dcd15f6094c5d817bc5757cdd6fde403
joaodematejr <joaodematejr@gmail.com>
2020-05-13 01:13:01 -0300

    switch

--- switch/switch/main.c
+++ switch/switch/main.c
@@ -8,8 +8,37 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, const char *argv[]){
+    int valor;
+
+    printf("Digite um valor de 1 a 7: ");
+    scanf("%d", &valor);
+
+    switch (valor){
+    case 1:
+        printf("Domingo \n");
+        break;
+    case 2:
+        printf("Segunda \n");
+        break;
+    case 3:
+        printf("Terça \n");
+        break;
+    case 4:
+        printf("Quarta \n");
+        break;
+    case 5:
+        printf("Quinta \n");
+        break;
+    case 6:
+        printf("Sexta \n");
+        break;
+    case 7:
+        printf("Sabado \n");
+        break;
+    default:
+        printf("Valor Invalido \n");
+        break;
+    }
     return 0;
 }
