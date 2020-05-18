8a5c1d652d034371d653c1eee31047824f3be6a6
joaodematejr <joaodematejr@gmail.com>
2020-05-17 23:19:39 -0300

    Matrizes parte 2

--- matrizesParte2/matrizesParte2/main.c
+++ matrizesParte2/matrizesParte2/main.c
@@ -6,10 +6,28 @@
 //  Copyright © 2020 João Dematé Jr 🤠. All rights reserved.
 //
 
+/*
+ 
+[00][01]
+[10][11]
+ 
+ */
+
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int numeros[2][2];
+    numeros[0][0] = 1;
+    numeros[0][1] = 2;
+    numeros[1][0] = 3;
+    numeros[1][1] = 4;
+    
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("Numeros [%d][%d] vale %d \n", i, j, numeros[i][j]);
+        }
+    }
+    
     return 0;
 }
