47250f685a4b456f84218c93051949062355725b
joaodematejr <joaodematejr@gmail.com>
2020-05-22 23:49:25 -0300

    Trabalhando com arquivos

--- entradasSaidas/entradasSaidas/main.c
+++ entradasSaidas/entradasSaidas/main.c
@@ -10,6 +10,16 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    FILE *arq;
+    //FOPEN(NOME-DO-ARQUIVO, FORMA-DE-ABERTURA-DO-ARQUIVO);
+    //W - ABRIR O ARQUIVO PARA ESCRITA
+    //R - ABRIR O ARQUIVO PARA LEITURA (NÃO PODEMOS ESCREVER NO ARQUIVO)
+    //WA ABRIR O ARQUIVO PARA ADIÇÃO DE CONTEÚDO(SE O ARQUIVO JÁ EXIBIR, O CONTEUDO SERÁ ADICIONADO NAS LINHAS ABAIXO)
+    arq = fopen("arquivo.txt", "w");
+    
+    // SEMPRE QUE A GENTE FINALIZAR A MANIPULAÇÃO DE UM ARQUIVO, DEVEMOS FECHÁ-LO
+    fclose(arq);
+    
+    
     return 0;
 }
