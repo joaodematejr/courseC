309acd625e7a53151fe694a9d26d3352f7667338
joaodematejr <joaodematejr@gmail.com>
2020-05-22 23:31:37 -0300

    Finalização ponteiros

--- ponteiro3/ponteiro3/main.c
+++ ponteiro3/ponteiro3/main.c
@@ -10,6 +10,17 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int valores[5] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; i++) {
+        printf("O valor %d tem %ld bytes \n", valores[i], sizeof(valores[i]));
+    }
+    printf("O Array valores possui %ld bytes \n", sizeof(valores));
+    
+    printf("valores[0] valor %d e endereço de memória é %p\n", valores[0], valores[0]);
+    printf("*(valores) vale %d e endereço de memória é %p\n", *(valores), *(valores));
+    printf("*(valores+1) vale %d e endereço de memória é %p\n", *(valores+1), *(valores+1));
+    printf("*(valores+2) vale %d e endereço de memória é %p\n", *(valores+2), *(valores+2));
+    printf("*(valores+3) vale %d e endereço de memória é %p\n", *(valores+3), *(valores+3));
+    printf("*(valores+4) vale %d e endereço de memória é %p\n", *(valores+4), *(valores+4));
     return 0;
 }
