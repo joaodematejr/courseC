ac27b639c0db1361bdb78cfa23871734bff12594
joaodematejr <joaodematejr@gmail.com>
2020-05-24 21:11:40 -0300

    Finalização arquivos

--- escritaEmArquivos/escritaEmArquivos/main.c
+++ escritaEmArquivos/escritaEmArquivos/main.c
@@ -7,9 +7,46 @@
 //
 
 #include <stdio.h>
+/*
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    FILE *arq;
+    char frutas[10];
+    
+    arq = fopen("frutas.txt", "w");
+        
+    if (arq) {
+        printf("Informe uma fruta, ou 0 para sair: \n");
+        fgets(frutas, 10, stdin);// STDIN = STANDARD INPUT => ENTRADA PADRÃO
+        while (frutas[0] != '0') {
+            fputs(frutas, arq);
+            printf("Informe uma fruta, ou 0 para sair: \n");
+            fgets(frutas, 10, stdin);
+        }
+    } else {
+        printf("Não foi possivel criar o arquivo.");
+    }
+    return 0;
+}
+ 
+ */
+
+int main(int argc, const char * argv[]) {
+    FILE *arq;
+    char frutas[10];
+    
+    arq = fopen("frutas.txt", "a");
+        
+    if (arq) {
+        printf("Informe uma fruta, ou 0 para sair: \n");
+        fgets(frutas, 10, stdin);// STDIN = STANDARD INPUT => ENTRADA PADRÃO
+        while (frutas[0] != '0') {
+            fputs(frutas, arq);
+            printf("Informe uma fruta, ou 0 para sair: \n");
+            fgets(frutas, 10, stdin);
+        }
+    } else {
+        printf("Não foi possivel criar o arquivo.");
+    }
     return 0;
 }
