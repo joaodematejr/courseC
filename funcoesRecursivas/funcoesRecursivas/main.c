a65e9be5d2d88daf7635d0fa539febeed4497bd6
joaodematejr <joaodematejr@gmail.com>
2020-05-26 01:00:17 -0300

    Finalização funções recursivas

--- funcoesRecursivas/funcoesRecursivas/main.c
+++ funcoesRecursivas/funcoesRecursivas/main.c
@@ -8,8 +8,35 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+//RECURSIVIDADE É O ATO DE UMA FUNÇÃO CHAMAR A SI MESMA
+//OBS: É IMPORTANTE QUE A FUNÇÃO RECURSIVA TENHA UMA CONDIÇÃO DE PARADA
+
+/*
+ 
+ 5 PRIMEIROS NÚMEROS DA SEQUÊNCIA FIBONACCI
+ 1 1 2 3 5
+ 
+*/
+
+int fib(int n){
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n -2);
+}
+
+
+int main() {
     // insert code here...
-    printf("Hello, World!\n");
+    int quantidade;
+    printf("Informe o tamanho da sequência fibonacci: ");
+    scanf("%d", &quantidade);
+    for (int i = 0; i < quantidade; i++) {
+        printf("%d ", fib(i + 1));
+    }
+    printf("\n");
     return 0;
 }
