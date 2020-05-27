96d7d2a8040bf302e147097d981a293f1ac51380
joaodematejr <joaodematejr@gmail.com>
2020-05-27 01:55:44 -0300

    Compilando, ifdef, ifndef, stdio

--- stdio/stdio/main.c
+++ stdio/stdio/main.c
@@ -6,10 +6,51 @@
 //  Copyright © 2020 João Dematé Jr 🤠. All rights reserved.
 //
 
+/*
+
+ FUNÇÕES DE ENTRADA E SAIDA:
+ ACESSO A ARQUIVOS;
+ 
+ fclose(); // FECHAR UM ARQUIVO JÁ ABERTO
+ fffush(); // FUNÇÃO LIMPAR BUFFER. (CORRIGE UM BUG DO WINDOWS + C + ECLIPSE)
+ fopen(); // FUNÇÃO ABRIR ARQUIVO
+ fscanf(); // LER DADOS DE UM ARQUIVO
+ prinf(); // IMPRIMIR DADOS
+ 
+ //ENTRADAS E SAIDAS DE CARACTERES
+ gets();
+ fputs();
+ getchar();
+ fgets();
+ 
+ //TRATAMENTO DE ERROS
+ feof();
+ 
+ //TIPOS DE MACROS
+ 
+ FILE
+ EOF
+ NULL
+ 
+*/
 #include <stdio.h>
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int num1, num2, soma;
+    
+    printf("Informe o num1: ");
+    fflush(stdout);
+    scanf("%d", &num1);
+    
+    printf("Informe o num2: ");
+    fflush(stdout);
+    scanf("%d", &num2);
+    
+    soma = num1 + num2;
+    
+    printf("A soma de %d com %d é %d \n", num1, num2, soma);
+    
     return 0;
 }
