2d882872907974da0e25d092ebfdb60ac9ebe01d
joaodematejr <joaodematejr@gmail.com>
2020-05-29 23:40:52 -0300

    time

--- time/time/main.c
+++ time/time/main.c
@@ -7,9 +7,44 @@
 //
 
 #include <stdio.h>
+#include <time.h>
+
+/*
+
+ FUNÇÕES DE DATA E HORA
+ 
+//clock(); RETORNA O NÚMERO DE PULSOS DE CLOCK DECORRIDO DESDE QUE O PROGRAMA FOI INICIADO
+//time() //RETORNA O TEMPO ATUAL DO CALENDÁRIO
+//localtime() // CONVERTE O VALOR TIME PARA UMA HORA LOCAL
+ 
+*/
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    /*
+    long int  inicio_t, fim_t, total_t;
+    inicio_t = clock();
+    printf("Realizar um loop grande, inicio_t = %ld\n", inicio_t);
+    for (int i = 0; i <= 1000000000; i++) {}
+    fim_t = clock();
+    printf("Fim do loop grande, fim_t = %ld \n", fim_t);
+    total_t = (double)(fim_t - inicio_t) / CLOCKS_PER_SEC;
+    printf("Clock por segundos: %ld \n", CLOCKS_PER_SEC);
+    printf("Tempo total usado pela CPU: %ld \n", total_t);
+    printf("Finalizando o programa");
+    */
+    
+    /*
+    time_t segundos;
+    segundos = time(NULL);
+    printf("Horas desde 1 de Janeiro de 1970 = %ld\n", segundos/3600);
+    */
+    
+    time_t tempobruto;
+    struct tm * info;
+    time(&tempobruto);
+    info = localtime(&tempobruto);
+    printf("Data e hora local %s", asctime(info));
+     
+     return 0;
 }
