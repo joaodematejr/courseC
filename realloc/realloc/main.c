da16821a3c0859ce75dd438e6fb6e50cdb755092
joaodematejr <joaodematejr@gmail.com>
2020-05-30 23:23:50 -0300

    realloc

--- realloc/realloc/main.c
+++ realloc/realloc/main.c
@@ -7,9 +7,37 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int qtd, *p;
+    
+    printf("Informe a quantidade de elementos para o vetor: ");
+    scanf("%d", &qtd);
+
+    
+    p = (int*)malloc(qtd * sizeof(int)); // 3 x 4  == 12 BYTES
+    
+    if (p) {
+        printf("A variável 'P' ocupa %ld bytes em memória. \n", qtd * sizeof(int));
+        
+        printf("Informe a quantidade de elementos para o vetor: ");
+        scanf("%d", &qtd);
+        
+        p = (int*) realloc(p, qtd * sizeof(int));
+        if (p) {
+            printf("A variável 'P' ocupa %ld bytes em memória. \n", qtd * sizeof(int));
+        }else{
+            printf("Erro: Memória insuficiente !!! \n");
+        }
+    }else{
+        printf("Erro: Memória insuficiente !!! \n");
+    }
+
+    //LIMPAR MEMORIA (DESALOCAR)
+    free(p);
+    p = NULL; //MEDIDA DE SEGURANÇA
     return 0;
 }
+
