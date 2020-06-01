05ca12c40f18bea2b88ac687a7ee28aeafdef0b0
joaodematejr <joaodematejr@gmail.com>
2020-05-31 23:01:35 -0300

    Array

---
+++ arrayMultidimensionais/arrayMultidimensionais/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  arrayMultidimensionais
+//
+//  Created by João Dematé Jr 🤠 on 31/05/20.
+//  Copyright © 2020 João Dematé Jr 🤠. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
